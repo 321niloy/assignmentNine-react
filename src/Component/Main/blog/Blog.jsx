04e179cdb213716c07951f11a6d6eb93ec2f2746
321niloy <niloyn670@gmail.com>
2023-04-11 21:38:22 +0600

    rechat and responsive

--- src/Component/Main/blog/Blog.jsx
+++ src/Component/Main/blog/Blog.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            <h1>Blog</h1>
+            <div className='shadow-md'><h1 className='font-extrabold'>When should use Contex Api???</h1>
+                 <p><span className='font-bold'>Ans:</span>You should use the React Context API when you have data
+                                     that needs to be accessed by multiple components at different
+                                      levels of the component tree, such as themes, authentication
+                                       status, localization, or as a replacement for Redux in small apps.
+                                        It helps avoid "prop drilling," but should be used judiciously to 
+                                        avoid making code harder to understand and maintain.</p>
+            </div>
         </div>
     );
 };
