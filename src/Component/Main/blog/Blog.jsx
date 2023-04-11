ed2978b4986fce1be4a4b11aab56a2801ad1a331
321niloy <niloyn670@gmail.com>
2023-04-11 21:52:46 +0600

    Thirdquestionsandans

--- src/Component/Main/blog/Blog.jsx
+++ src/Component/Main/blog/Blog.jsx
@@ -16,6 +16,13 @@ const Blog = () => {
                                                            stateful logic across multiple components. It allows you to abstract complex logic into <br />
                                                             reusable functions that can be shared throughout your application.</p>
             </div>
+
+            <div className='shadow-md mt-10'><h1 className='font-extrabold'>what is UseReference in react???????</h1>
+                 <p><span className='font-bold'>Ans:</span>useRef is a built-in hook in React that returns a mutable ref object. Its primary use case is <br /> 
+                                                           to access DOM nodes or other mutable values that are managed outside the component's state. <br />
+                                                           The .current property of the ref object can hold a value across renders and can be used to access <br />
+                                                            or modify the referenced value.</p>
+            </div>
         </div>
     );
 };
